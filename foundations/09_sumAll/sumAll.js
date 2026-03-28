63248ac84875a032ab4e1a002a44d7bae2f619df
aki <247784740+dzakinnwa@users.noreply.github.com>
2026-03-28 08:23:20 +0800

    practice loops and arrays foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let arr = [];
+    let min = num1;
+    let max = num2;
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)
+        || num1 < 0 || num2 < 0
+    ) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        min = num2;
+        max = num1;
+    }
+
+    for (let i = min; i <= max; i++) {
+        arr.push(i);
+    }
+    const result = arr.reduce((total, current) => total + current);
+    return result;
 };
+
+console.log(sumAll(3, 7))
 
 // Do not edit below this line
 module.exports = sumAll;
